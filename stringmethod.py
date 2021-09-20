5e5d82d9d6103da160d3b645a3808eff43c8aa1c
Manish241099 <86618105+Manish241099@users.noreply.github.com>
2021-09-20 11:42:14 +0530

    Add files via upload

---
+++ stringmethod.py
@@ -0,0 +1,28 @@
+str="nothing is possible in this curse world"
+#capitalize method
+x=str.capitalize()
+print(x)
+
+#casefold method
+y=str.casefold()
+print(y)
+
+#center method
+a=str.center(200)
+print(a)
+
+#count method
+b=str.count("curse")
+print(b)
+
+#encode method
+c=str.encode()
+print(c)
+
+#lower method
+d=str.lower()
+print(d)
+
+#upper method
+u=str.upper()
+print(u)
